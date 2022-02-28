86b49377665bf0caefe154a143c661bb2f6b4ea5
zukeyama0424 <zukeyama99@gmail.com>
2022-02-28 22:24:02 +0900

    add: storybook サンプル追加

---
+++ components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'atoms/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'Button',
+};

ebb7489df2f4b16d33b8a0c29b6ff3e130eeccfc
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-01 22:10:36 +0900

    feat: CheckboxコンポーネントのStoryを追加

---
+++ src/components/atoms/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import { css } from "@emotion/css";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Checkbox } from "./Checkbox";
+
+export default {
+    title: "atoms/Checkbox",
+    component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+const Template: ComponentStory<typeof Checkbox> = (args) => {
+    return (
+        <div className={css({padding: 10})}>
+                <Checkbox {...args} />
+        </div>
+    )
+}
+
+export const Dafult = Template.bind({});
+
+Dafult.args = {
+    children: "同意する"
+}

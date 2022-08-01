e037a1b0c45d9afdcaaef59cc7ada6bcfeb91b96
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-01 13:20:52 +0900

    fix: PrimaryButtonコンポーネントのPropsの型を修正

--- src/components/atoms/Button/PrimaryButton.tsx
+++ src/components/atoms/Button/PrimaryButton.tsx
@@ -1,13 +1,12 @@
 import { css } from "@emotion/css"
 import { classNames } from "../../../helper/style"
-import { ButtonProps } from "./Button"
-import { ButtonCore } from "./ButtonCore"
+import { ButtonCore, ButtonCoreProps } from "./ButtonCore"
 
 /**
  * このコンポーネントを使用する場合、
  * `Button`コンポーネントの`type="primary"`を利用してください
  */
-export const PrimaryButton: React.FC<ButtonProps> = ({className, ...props}) => {
+export const PrimaryButton: React.FC<ButtonCoreProps> = ({className, ...props}) => {
     return (
         <ButtonCore {...props} className={classNames([styles.primary, className])} />
     )

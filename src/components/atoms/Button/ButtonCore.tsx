ace105917d73a79d05f26c18aee0f2161834f91b
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-01 21:34:23 +0900

    feat: Buttonコンポーネントにiconのpropsを追加

--- src/components/atoms/Button/ButtonCore.tsx
+++ src/components/atoms/Button/ButtonCore.tsx
@@ -1,11 +1,32 @@
+import { css } from "@emotion/css"
 import React from "react"
+import { IconBaseProps } from "react-icons"
+import { classNames } from "../../../helper/style"
+import { Icon, icon } from "../Icon/Icon"
 
 export type ButtonCoreProps = {
   children?: React.ReactNode
   onClick?: () => void
   className?: string
+  startIcon?: icon
+  endIcon?: icon
+  iconProps?: IconBaseProps
 }
 
-export const ButtonCore: React.FC<ButtonCoreProps> = ({children, onClick, className}) => {
-    return <button onClick={onClick} className={className}>{children}</button>
+export const ButtonCore: React.FC<ButtonCoreProps> = ({children, onClick, className, startIcon, endIcon, iconProps}) => {
+    return (
+        <button onClick={onClick} className={classNames([styles.button, className])}>
+            {startIcon && <Icon {...iconProps} name={startIcon} />}
+            {children}
+            {endIcon && <Icon {...iconProps} name={endIcon} />}
+        </button>
+    )
+}
+
+const styles = {
+    button: css({
+        display: "flex",
+        alignItems: "center",
+        gap: 4
+    })
 }

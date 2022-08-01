59d682b09d44fd80cabe176f324bfa6ed4451ded
daiti0113 <stennis.ds.0113@gmail.com>
2022-08-01 13:12:09 +0900

    feat: ButtonコンポーネントのStoryを追加

---
+++ src/components/atoms/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Button } from './Button'
+
+export default {
+    title: 'atoms/Button',
+    component: Button,
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = (args) => {
+    return <Button {...args} />
+}
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+    children: "ボタン",
+}
